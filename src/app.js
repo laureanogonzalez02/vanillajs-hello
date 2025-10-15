08c0acc3042e68bea0da21b86430b8be86f39626
Laureano <laugx2002@hotmail.com>
2025-10-14 23:13:08 -0300

    Generador de excusas hecho

--- src/app.js
+++ src/app.js
@@ -1,11 +1,51 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const who = ["The dog", "My grandma", "The mailman", "My bird"];
+const action = ["ate", "peed", "crushed", "broke"];
+const what = ["my homework", "my phone", "the car"];
+const when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying",
+];
+
+//crea numeros aleatorios para elegir las partes de la excusa
+function indexSelector() {
+  let randomWhoAndAction = Math.floor(Math.random() * (3 - 0 + 1)) + 0;
+  let randomIndexWhat = Math.floor(Math.random() * (2 - 0 + 1)) + 0;
+  let randomIndexWhen = Math.floor(Math.random() * (4 - 0 + 1)) + 0;
+  return [randomWhoAndAction, randomIndexWhat, randomIndexWhen];
+}
+
+//usa los numeros para generar la excusa
+function excuseGenerator() {
+  const [indexWho] = indexSelector();
+  const [indexAction] = indexSelector();
+  const [, indexWhat] = indexSelector();
+  const [, , indexWhen] = indexSelector();
+  let excusa = [
+    who[indexWho],
+    action[indexAction],
+    what[indexWhat],
+    when[indexWhen],
+  ];
+  let p = document.querySelector(".excuses");
+  p.textContent = excusa.join(" ");
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  window.addEventListener("load", () => {
+    document.body.style.visibility = "visible";
+  });
+  const boton = document.querySelector(".boton");
+  boton.addEventListener("click", () => {
+    location.reload();
+  });
+  excuseGenerator();
+});
